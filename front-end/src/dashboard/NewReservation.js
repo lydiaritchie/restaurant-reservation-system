828f238926ff3f44f3f7b643eb9bca20147996fa
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2024-08-22 14:27:29 +0700

    Added state management of NewReservation form.

--- front-end/src/dashboard/NewReservation.js
+++ front-end/src/dashboard/NewReservation.js
@@ -6,52 +6,93 @@ import React, { useEffect, useState } from "react";
  */
 
 function NewReservations() {
+  const initialFormState = {
+    first_name: "",
+    last_name: "",
+    mobile_number: "",
+    reservation_date: "",
+    reservation_time: "",
+    people: 0,
+  };
+  
+  const [formData, setFormData] = useState({ ...initialFormState });
+
+  //handle change to form inputs
+  function handleChange({ target }) {
+    setFormData({
+      ...formData,
+      [target.name]: target.value,
+    });
+    console.log(target.name + ": " + target.value);
+  }
+
   return (
     <main>
       <h2>Create Reservation</h2>
       <form className="col">
         <label className="form-components">
           First Name:
-          <input name="first_name" />
+          <input
+            name="first_name"
+            onChange={handleChange}
+            value={formData.name}
+          />
         </label>
 
         <label className="form-components">
           Last Name:
-          <input name="last_name" />
+          <input
+            name="last_name"
+            onChange={handleChange}
+            value={formData.name}
+          />
         </label>
 
-        <label className="form-components">
+        <label className="form-components fs-5">
           Phone Number:
-          <input name="mobile_number" />
+          <input
+            name="mobile_number"
+            onChange={handleChange}
+            value={formData.name}
+            maxLength={10}
+          />
         </label>
 
         <label className="form-components">
           Date:
-          <input 
-          name="reservation_date" 
-          type="date"
+          <input
+            name="reservation_date"
+            type="date"
+            onChange={handleChange}
+            value={formData.name}
           />
         </label>
 
         <label className="form-components">
           Time:
-          <input name="reservation_time" type="time" />
+          <input
+            name="reservation_time"
+            type="time"
+            onChange={handleChange}
+            value={formData.name}
+          />
         </label>
 
         <label className="form-components">
           Number of people:
-          <input 
-          name="people"
-          type="number"
-          min="0"
-           />
+          <input
+            name="people"
+            type="number"
+            min="0"
+            onChange={handleChange}
+            value={formData.name}
+          />
         </label>
 
         <div className="buttons justify-content-between">
           <button className="btn btn-light">Cancel</button>
-          <button className="btn btn-primary">Submit</button>
+          <button className="btn purple-button">Submit</button>
         </div>
-
       </form>
     </main>
   );
