e1dede316e680d51b191e6672255ff7217848e5a
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2024-09-02 06:24:39 +0700

    Started adding validation for setReservation

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -1,3 +1,4 @@
+const P = require("pino");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 const service = require("./tables.service");
 
@@ -19,22 +20,58 @@ async function list(req, res, next) {
   }
 }
 
+/**
+ * setReservation validations
+ * table capacity less than number of people in reservation, return 400 & error message
+ * if the table is occupied, return 400 & error message
+ */
+
+async function validateSetReservation(req, res, next) {
+  const reservation_id = req.body.data;
+  const table_id = req.params.table_id;
+
+  // Attach the values to the req object
+  req.reservation_id = reservation_id;
+  req.table_id = table_id;
+
+  //get table
+  try{
+    const currentTable = await service.read(table_id);
+  } catch (error){
+    console.log("error:", error);
+    throw error;
+  }
+
+  //if table is occupied
+  if(currentTable.reservation_id != null){
+    next({status: 400, message: "This table is already occupied"});
+  }
+
+  next();
+}
+
 async function setReservation(req, res, next) {
-    const reservation_id = req.body.data;
-    console.log("reservation_id:", reservation_id);
-    const table_id = req.params.table_id;
-    console.log(table_id);
+  const reservation_id = req.reservation_id;
+  console.log("reservation_id:", reservation_id);
+  const table_id = req.table_id;
+  console.log(table_id);
   try {
-    const updatedTable = await service.setTableReservation(table_id, reservation_id);
-    res.status(200).json({ data: 7 });
+    const updatedTable = await service.setTableReservation(
+      table_id,
+      reservation_id
+    );
+    res.status(200).json({ data: updatedTable });
   } catch (error) {
     console.log(error);
-    next({status: 400, message: "Could not update the table"});
+    next({ status: 400, message: "Could not update the table" });
   }
 }
 
 module.exports = {
   create: [asyncErrorBoundary(create)],
   list: [asyncErrorBoundary(list)],
-  setTableReservation: [asyncErrorBoundary(setReservation)],
+  setTableReservation: [
+    asyncErrorBoundary(validateSetReservation),
+    asyncErrorBoundary(setReservation),
+  ],
 };
--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -3,37 +3,42 @@ const knex = require("../db/connection");
 
 const tableName = "tables";
 
-async function createTable(newTable){
-    return knex(tableName)
-        .insert(newTable)
-        .returning("*")
-        .then((createdRecords) => createdRecords[0]);
+async function createTable(newTable) {
+  return knex(tableName)
+    .insert(newTable)
+    .returning("*")
+    .then((createdRecords) => createdRecords[0]);
 }
 
-async function listTables(){
-    return knex(tableName)
+async function listTables() {
+  return knex(tableName)
     .select("*")
     .orderBy("table_name", "asc")
     .then((tables) => tables)
     .catch((error) => {
-        console.log("Error listing tables:", error);
-        throw error;
-    })
+      console.log("Error listing tables:", error);
+      throw error;
+    });
 }
 
-async function setTableReservation(table_id, new_reservation_id){
- return await knex(tableName)
-        .where({table_id})
-        .update({reservation_id: new_reservation_id})
-        .returning("*")
-        .catch((error) => {
-            console.log(`Could not update table ${table_id}`)
-            throw error;
-        })
+async function read(table_id) {
+  return knex(tableName).select("*").where({ table_id }).first();
 }
 
-module.exports ={
-    createTable,
-    listTables,
-    setTableReservation,
+async function setTableReservation(table_id, new_reservation_id) {
+  return await knex(tableName)
+    .where({ table_id })
+    .update({ reservation_id: new_reservation_id })
+    .returning("*")
+    .catch((error) => {
+      console.log(`Could not update table ${table_id}`);
+      throw error;
+    });
 }
+
+module.exports = {
+  createTable,
+  listTables,
+  setTableReservation,
+  read
+};
